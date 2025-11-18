eb77c1e3d10dd2cbccb9efec1297a9e163f8d2aa
J-Huster <johanna_huster@yahoo.com>
2025-11-18 14:56:07 -0700

    update code

--- piano_stairs_controller.py
+++ piano_stairs_controller.py
@@ -1,8 +1,11 @@
 import RPi.GPIO as GPIO
 import time
 import piano_stairs_model as model
-import playsound
-import pygobject
+
+import pygame
+pygame.mixer.init()
+
+#import os
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -15,27 +18,34 @@ GPIO.output(TRIG, False)
 GPIO.setup(ECHO, GPIO.IN)
 time.sleep(2)
 
-try:
-    while True:
-        # sensor one
-        sensorNum = 1
-        # send out pulse signal
-        GPIO.output(TRIG, True)
-        time.sleep(0.00001)
-        GPIO.output(TRIG, False)
+#try:
+while True:
+    # sensor one
+    sensorNum = 1
+    # send out pulse signal
+    GPIO.output(TRIG, True)
+    time.sleep(0.00001)
+    GPIO.output(TRIG, False)
 
-        # will record last time echo is low
-        while GPIO.input(ECHO)==0:
-            pulse_start = time.time()
+    # will record last time echo is low
+    while GPIO.input(ECHO) == 0:
+        pulse_start = time.time()
 
-        # will record last time echo is high
-        while GPIO.input(ECHO)==1:
-            pulse_end = time.time()       
+    # will record last time echo is high
+    while GPIO.input(ECHO) == 1:
+        pulse_end = time.time()       
 
-        distance = model.getDistance(pulse_start, pulse_end)
-        trigger = model.isTriggered(distance)
-        #sound = model.getSound(sensorNum)
-        playsound('/home/johaha/Desktop/test_sound.py')
+    distance = model.getDistance(pulse_start, pulse_end)
+    print(distance)
+    trigger = model.isTriggered(distance) 
+    sound = model.getSound(1)
         
-except:
-    GPIO.cleanup()
+    
+    #pygame.mixer.music.load("test_sound.mp3")
+    #pygame.mixer.music.play()
+    #time.sleep(2)
+    if trigger:
+        model.playSound()
+
+#except:
+    #GPIO.cleanup()
--- piano_stairs_model.py
+++ piano_stairs_model.py
@@ -1,3 +1,7 @@
+import time
+import pygame
+pygame.mixer.init()
+
 def getSound():
     return "sound"
 
@@ -7,12 +11,17 @@ def getDistance(start, end):
     return distance
 
 def isTriggered(x):
-    if x < 300:
+    if x < 50:
         return True
     return False
 
 def getSound(num):
     if num == 1:
-        return "sond.mp3"
+        return "/home/johaha/Desktop/Piano-Stairs/test_sound.mp3"
     else:
         return None
+
+def playSound():
+    pygame.mixer.music.load("test_sound.mp3")
+    pygame.mixer.music.play()
+    time.sleep(2)
