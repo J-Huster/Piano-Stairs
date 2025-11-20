a7f4cd2de66a33fdd446fb697ce135c629679c35
J-Huster <johanna_huster@yahoo.com>
2025-11-20 15:07:22 -0700

    update code

--- piano_stairs_controller.py
+++ piano_stairs_controller.py
@@ -1,7 +1,6 @@
 import RPi.GPIO as GPIO
 import time
 import piano_stairs_model as model
-
 import pygame
 pygame.mixer.init()
 
@@ -10,42 +9,74 @@ pygame.mixer.init()
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-TRIG = 21
-ECHO = 12
+# set up sensor number variables
+trig1 = 21  # sends a pulse to iniate measurement
+echo1 = 12  # measures the time for pulse to return after pucing off the closest object
 
-GPIO.setup(TRIG, GPIO.OUT)
-GPIO.output(TRIG, False)
-GPIO.setup(ECHO, GPIO.IN)
-time.sleep(2)
+GPIO.setup(trig1, GPIO.OUT)  # sets up trig sensor as output
+GPIO.output(trig1, False)  # sets output to be false
+GPIO.setup(echo1, GPIO.IN)  # sets up echo sensor as input
+time.sleep(.01)  # pauses program for .01 second
 
-#try:
+trig2 = 16
+echo2 = 20
+
+GPIO.setup(trig2, GPIO.OUT)  # sets up trig sensor as output
+GPIO.output(trig2, False)  # sets output to be false
+GPIO.setup(echo2, GPIO.IN)  # sets up echo sensor as input
+time.sleep(.01)  # pauses program for .01 second
+
+trig3 = 1
+echo3 = 1
+
+trig4 = 1
+echo4 = 1
+
+trig5 = 1
+echo5 = 1
+
+trig6 = 1
+echo6 = 1
+
+trig7 = 1
+echo7 = 1
+
+trig8 = 1
+echo8 = 1
+
+sensors = [trig1, echo1, trig2, echo2]
+
+# sets up all of the sensors
+#for s in range(len(sensors)):
+#    if s % 2 == 0:
+#        GPIO.setup(s, GPIO.OUT)  # sets up trig sensor as output
+#        GPIO.output(s, False)  # sets output to be false
+#    if s % 2 != 0:
+#        GPIO.setup(s, GPIO.IN)  # sets up echo sensor as input
+#    time.sleep(.01)  # pauses program for .01 second
+
 while True:
-    # sensor one
-    sensorNum = 1
-    # send out pulse signal
-    GPIO.output(TRIG, True)
-    time.sleep(0.00001)
-    GPIO.output(TRIG, False)
-
-    # will record last time echo is low
-    while GPIO.input(ECHO) == 0:
-        pulse_start = time.time()
-
-    # will record last time echo is high
-    while GPIO.input(ECHO) == 1:
-        pulse_end = time.time()       
-
-    distance = model.getDistance(pulse_start, pulse_end)
-    print(distance)
-    trigger = model.isTriggered(distance) 
-    sound = model.getSound(1)
+    for n in range(0, len(sensors), 2):
+        TRIG = sensors[n]
+        # send out pulse signal
+        GPIO.output(TRIG, True)
+        time.sleep(0.00001)
+        GPIO.output(TRIG, False)
         
-    
-    #pygame.mixer.music.load("test_sound.mp3")
-    #pygame.mixer.music.play()
-    #time.sleep(2)
-    if trigger:
-        model.playSound()
+        n += 1
+        ECHO = sensors[n]
+        # will record last time echo is low
+        while GPIO.input(ECHO) == 0:
+            pulse_start = time.time()
 
-#except:
-    #GPIO.cleanup()
+        # will record last time echo is high
+        while GPIO.input(ECHO) == 1:
+            pulse_end = time.time()       
+
+        distance = model.getDistance(pulse_start, pulse_end)
+        print(distance)
+        trigger = model.isTriggered(distance)
+        sound = model.getSound(n)
+        
+        if trigger:
+            model.playSound(sound)
--- piano_stairs_model.py
+++ piano_stairs_model.py
@@ -2,26 +2,25 @@ import time
 import pygame
 pygame.mixer.init()
 
-def getSound():
-    return "sound"
-
 def getDistance(start, end):
     duration = end - start
     distance = 17150 * (duration / 2)
     return distance
 
 def isTriggered(x):
-    if x < 50:
+    if x < 10:
         return True
     return False
 
 def getSound(num):
     if num == 1:
         return "/home/johaha/Desktop/Piano-Stairs/test_sound.mp3"
+    elif num == 3:
+        return "/home/johaha/Desktop/Piano-Stairs/test_sound_2.mp3"
     else:
         return None
 
-def playSound():
-    pygame.mixer.music.load("test_sound.mp3")
+def playSound(sound):
+    pygame.mixer.music.load(sound)
     pygame.mixer.music.play()
-    time.sleep(2)
+    time.sleep(1)
